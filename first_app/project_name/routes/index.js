a16567e26d551263b0c488c196c650d017a34bdd
frank850601 <frank850601@gmail.com>
2018-04-23 23:46:58 -0400

    updated

--- first_app/project_name/routes/index.js
+++ first_app/project_name/routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+const app = express();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+app.get('/', function(req, res){
+  res.send('hello world');
+});
+
+app.listen(3000, function() {
+  console.log('Example app is running on port 3000!');
+});
+
 module.exports = router;
+
